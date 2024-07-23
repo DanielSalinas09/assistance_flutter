340e312c134192c9578369cec4fddc153df29503
Daniel Salinas <danielsalinassalinas09@gmail.com>
2024-07-23 01:28:09 -0500

    login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -8,8 +8,68 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Imagen en la parte superior
+            Image.asset(
+              'assets/unilibre.png',
+              height: 150, // Ajusta el tamaño según sea necesario
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 20), // Espacio entre la imagen y el formulario
+            // Formulario
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(labelText: 'Username'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a username';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                    
+                    },
+                    child: Text('Iniciar sesíon'),
+                  ),
+                  SizedBox(height: 20),
+                  TextButton(onPressed: (){}, 
+                  child: Text('¿Has olvidado tu contraseña?'),)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
